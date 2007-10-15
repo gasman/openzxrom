5c014006c57100dbb5753a4408c05249929703ae
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2007-10-15 22:14:20 +0000

    Tag as release 2007-10-15

--- openzxrom/splash_screen.asm
+++ openzxrom/splash_screen.asm
@@ -1,7 +1,7 @@
 			db 0x11, 0x06	; paper yellow
 			db "+------------------------------+"
-			db "| OpenZXRom v2006-06-18        |"
-			db "| ", 0x7f, " 2006 OpenZXRom Project     |"
+			db "| OpenZXRom v2007-10-15        |"
+			db "| ", 0x7f, " 2007 OpenZXRom Project     |"
 			db "+------------------------------+"
 			db 0x11, 0x07, 0x10, 0x01		; paper white, ink blue
 			db 0x0d							; new line
